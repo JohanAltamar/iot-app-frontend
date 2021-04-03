03e4e7edbfc144bc2ec11a2f795703b4aab52a80
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-03 18:03:15 -0500

    project created & socket client set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import io from 'socket.io-client';
 
 function App() {
+  useEffect(() => {
+    const socketClient = io("http://localhost:8080", {
+      transports: ['websocket', 'polling', 'flashsocket'], 'auth': {
+        user: 'TEST'
+      }
+    })
+    
+    socketClient.on("connect", () => {
+      console.log("Connected")
+    })
+
+    socketClient.on("message", payload => {
+      console.log(payload)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hola
     </div>
   );
 }
