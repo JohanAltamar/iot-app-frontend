d30571f23a6eb0a658d3f86d19222a757daecef3
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-04 14:05:03 -0500

    Wrap component with MainContainer component

--- src/router/ProtectedRoute.js
+++ src/router/ProtectedRoute.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import MainContainer from '../components/Containers/Main';
 
 const ProtectedRoute = ({ component: Component, user, ...rest }) => {
   return (
     <Route {...rest} render={
       props => {
         if (user) {
-          return <Component {...rest} {...props} />
+          return (
+            <MainContainer>
+              <Component {...rest} {...props} />
+            </MainContainer>
+          )
         } else {
           return <Redirect to={
             {
