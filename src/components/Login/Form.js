d298936b0c0014c7723326e8b15031eb02105082
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-03 22:56:34 -0500

    Add login form component

---
+++ src/components/Login/Form.js
@@ -0,0 +1,125 @@
+import { useForm, Controller } from 'react-hook-form';
+import { Avatar, Button, Grid, TextField, Typography, Link, Box } from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import Copyright from "../Copyright"
+import { loginUser } from '../../api/users';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1500&q=80)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Form = () => {
+  const classes = useStyles();
+  const { handleSubmit, control } = useForm({ mode: "onBlur" });
+
+  const onSubmit = async (data) => {
+    await loginUser(data);
+  };
+
+  return (
+    <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <form
+        className={classes.form}
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+      >
+        <Controller
+          name="email"
+          as={
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              autoComplete="email"
+              autoFocus
+            />
+          }
+          control={control}
+          defaultValue=""
+          rules={{
+            required: 'Required'
+          }}
+        />
+        <Controller
+          name="password"
+          as={
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          }
+          control={control}
+          defaultValue=""
+          rules={{
+            required: 'Required'
+          }}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link href="#" variant="body2">
+              Forgot password?
+                </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </form>
+    </div>
+  )
+}
+
+export default Form
