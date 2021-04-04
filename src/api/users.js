5228cac6a5078be33c25cc7b458d7570952444cf
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-04 01:10:11 -0500

    add fetch user info

--- src/api/users.js
+++ src/api/users.js
@@ -6,14 +6,34 @@ const apiUrl = process.env.REACT_APP_API_URL;
 export const loginUser = async (userInfo) => {
   try {
     const res = await axios.post(`${apiUrl}/api/auth/login`, { ...userInfo })
-    return (res.data);
+    return res.data;
   } catch (error) {
+    errorHandler(error)
+  }
+}
+
+export const fetchUserInfo = async (token) => {
+  try {
+    const res = await axios.get(`${apiUrl}/api/users/get-info`, {
+      headers: {
+        "x-session-key": token
+      }
+    })
+    return res.data
+  } catch (error) {
+    errorHandler(error)
     if (error.response) {
-      errorAlert(error.response.data.msg)
-    } else if (error.request) {
-      errorAlert("Check your internet connection")
-    } else {
-      errorAlert(error.message)
+      localStorage.removeItem("sessionToken")
     }
+  }
+}
+
+const errorHandler = (error) => {
+  if (error.response) {
+    errorAlert(error.response.data.msg)
+  } else if (error.request) {
+    errorAlert("Check your internet connection")
+  } else {
+    errorAlert(error.message)
   }
 }
