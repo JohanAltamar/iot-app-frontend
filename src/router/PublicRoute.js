59052fb5462d38adc772ba0026085ff703f0be42
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-04 01:17:47 -0500

    update protected route

--- src/router/PublicRoute.js
+++ src/router/PublicRoute.js
@@ -3,13 +3,15 @@ import { Route, Redirect, useLocation } from 'react-router-dom';
 
 const PublicRoute = ({ component: Component, user, ...rest }) => {
   const location = useLocation()
+  const pathname = location?.state?.from?.pathname || "/"
+  
   return (
     <Route {...rest} render={
       props => {
         if (!user) {
           return <Component {...rest} {...props} />
         } else {
-          return <Redirect to={location.state.from?.pathname || "/"} />
+          return <Redirect to={pathname} />
         }
       }
     } />
