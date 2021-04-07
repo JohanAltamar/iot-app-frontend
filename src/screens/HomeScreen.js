b141a0582723656dae45dd6985816a5efd1a2341
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-06 22:42:11 -0500

    update home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,39 @@
+import { useContext, useEffect, useState } from "react"
+import UserContext from "../context/UserContext"
+import { getRooms } from "../api/users"
+import RoomsCards from "../components/Containers/RoomCards"
+
 const HomeScreen = () => {
+  const { userInfo, sessionToken } = useContext(UserContext)
+
+  const [rooms, setRooms] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUserRooms = async () => {
+      if (sessionToken && userInfo) {
+        const resp = await getRooms(sessionToken, userInfo.uid)
+        if (resp.results) {
+          setRooms(resp.results)
+        }
+      }
+      setLoading(false);
+    }
+    getUserRooms();
+  }, [sessionToken, userInfo])
+
   return (
-    <div>
-      home
-    </div>
+    loading ? <h2>Cargando ...</h2> : (
+      <>
+        {
+          !!rooms.length ? (
+            <RoomsCards rooms={rooms}/>
+          ) : (
+            <h3>There are no rooms attached! Contact your administrator.</h3>
+          )
+        }
+      </>
+    )
   )
 }
 
